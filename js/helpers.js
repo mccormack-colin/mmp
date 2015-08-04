f398d064f46e9d429858af3b96a6aa564b42440d
Colin McCormack <mccormack.colin@gmail.com>
2015-08-04 14:38:49 -0400

    rework for v2

---
+++ js/helpers.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {;
+	$('a[href^="#"]').click(function(event){
+	    event.preventDefault();
+	    $('html, body').animate({ scrollTop: $( $.attr(this, 'href') ).offset().top }, 700);
+	});
+});
+
+
